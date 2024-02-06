a98c2ef95590fc7456045f9a304f44b118ccf6c9
Vasjakot5 <Vasja.rudnih@gmail.com>
2024-02-06 07:31:14 +0300

    Two Edit

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from datetime import date
 print(f"Сегодня {date.today()}")
+
+import datetime
+
+today = datetime.date.today()
+tomorrow = today + datetime.timedelta(days=1)
+
+print(tomorrow.strftime('завтра %d.%m'))
